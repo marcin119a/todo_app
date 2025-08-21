8e479e32833695c137ca8efb818fc6fadac9c058
Marcin Wierzbiński <marcin119a@gmail.com>
2025-08-21 15:25:57 +0200

    project/models.py

--- project/models.py
+++ project/models.py
@@ -11,3 +11,17 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProjectComment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='project_comments', null=True, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"Komentarz do projektu: {self.project.name} ({self.created_at:%Y-%m-%d %H:%M})"
+
+    class Meta:
+        ordering = ['-created_at']
